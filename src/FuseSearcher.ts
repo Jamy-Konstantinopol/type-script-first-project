966b78e7f3d94f8b1e13d11009e2e0e7ec34dd3e
jaimy.const <piskakosti@gmail.com>
2023-12-16 01:27:07 +0300

    супер дупер пупер последний коммит

--- src/FuseSearcher.ts
+++ src/FuseSearcher.ts
@@ -3,21 +3,15 @@ import { BaseSearcher } from "./BaseSearcher";
 import { IndexData } from "./IndexData";
 
 
-export class FuseSearcher implements BaseSearcher<FuseResult<unknown>>
+export class FuseSearcher implements BaseSearcher<FuseResult<IndexData>>
 {
 	private _indexes: IndexData[] = [];
 	private _fuse: Fuse<IndexData>;
 
 	constructor(datas : any[])
 	{
-		this._indexes = this._initIndexes(datas);
+		this._indexes = [...datas];
 		this._fuse = new Fuse(this._indexes, { keys: ["link", "title", "body"] });
-	}
-
-	private _initIndexes(datas: any[])
-	{
-		const indexes = [...datas];
-		return indexes;
 	}
 
 	public search(query: string): FuseResult<IndexData>[]
